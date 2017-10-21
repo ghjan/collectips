628c4fb0ff189cfe64c4095302d31b69c11e4061
David Zhang <cajan2@163.com>
2017-10-21 23:11:22 +0800

    modify spider;

--- collectips/spiders/ip_spider.py
+++ collectips/spiders/ip_spider.py
@@ -21,11 +21,11 @@ class IpSpiderSpider(scrapy.Spider):
         items = []
         for tr in trs:
             item = CollectipsItem()
-            item.IP = tr.xpath('td[2]/text()').extract_first()
-            item.PORT = tr.xpath('td[3]/text()').extract_first()
-            item.POSITION = tr.xpath('td[4]/text()').extract_first()
-            item.TYPE = tr.xpath('td[6]/text()').extract_first()
-            item.SPEED = tr.xpath('td[7]/div/@title').re_first('\d{0,2}\.\d{0,}')
-            item.LAST_CHECK_TIME = tr.xpath('td[10]/text()').extract_first()
+            item['IP'] = tr.xpath('td[2]/text()').extract_first()
+            item['PORT'] = tr.xpath('td[3]/text()').extract_first()
+            item['POSITION'] = tr.xpath('td[4]/text()').extract_first()
+            item['TYPE'] = tr.xpath('td[6]/text()').extract_first()
+            item['SPEED'] = tr.xpath('td[7]/div/@title').re_first('\d{0,2}\.\d{0,}')
+            item['LAST_CHECK_TIME'] = tr.xpath('td[10]/text()').extract_first()
             items.append(item)
         return items
